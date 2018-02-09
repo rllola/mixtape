91072401cb2f84d18e0944204be78f29b45df9ae
rllola <rllola80@gmail.com>
2018-02-09 21:07:03 +0100

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "===== INSTALL DEPENDENCIES ====="
+apt install msgpack-python python-gevent wget
+
+echo "===== INSTALL ZERONET ====="
+wget https://github.com/HelloZeroNet/ZeroNet/archive/master.tar.gz
+tar xvpfz master.tar.gz
+cd ZeroNet-master
+python zeronet.py help
