33599f30978b70c137fd667294d6df74a5c71818
rllola <rllola80@gmail.com>
2018-02-20 15:37:30 +0100

    New attempt

--- scripts/provide.sh
+++ scripts/provide.sh
@@ -11,7 +11,11 @@ tar xvpfz master.tar.gz
 echo "==== COPY FILES ===="
 mkdir ZeroNet-master/data
 mkdir ZeroNet-master/data/1MzSDy3x8cFrt3P2YkXNN7ogYDFfKX6hnP
+
+cd mixtape
 ls -A
+cd ..
+
 mv mixtape/* ZeroNet-master/data/1MzSDy3x8cFrt3P2YkXNN7ogYDFfKX6hnP/
 
 echo "==== BUILD DIST ===="
