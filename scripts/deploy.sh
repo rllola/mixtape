9071f3d2e23672537960ef0294f38882098fed54
rllola <rllola80@gmail.com>
2018-02-20 21:13:15 +0100

    LOL

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 echo "===== DEPLOY ====="
+pwd
 cd ../../
+ls
 python zeronet.py peerPing 51.15.214.168
 python zeronet.py siteSign --ppublish 1MzSDy3x8cFrt3P2YkXNN7ogYDFfKX6hnP $ZERONET_KEY_STAGING
 
