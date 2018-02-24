7deabf85074e8471bcf494e980e9b656eb592ced
Lola Dam <rllola80@gmail.com>
2018-02-25 00:37:46 +0100

    Limit file size (1MB) + define accepted files in form (#7)

--- app/scenes/Player/components/Upload/UploadForm.js
+++ app/scenes/Player/components/Upload/UploadForm.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
-import { Form, Loader, Image, Modal, Grid, Dimmer, Segment } from 'semantic-ui-react'
+import {
+  Form,
+  Loader,
+  Image,
+  Modal,
+  Grid,
+  Dimmer,
+  Message
+} from 'semantic-ui-react'
 import { inject } from 'mobx-react'
 
 @inject('site')
@@ -15,9 +23,10 @@ class UploadForm extends Component {
     this.state = {
       artist: '',
       title: '',
-      srcThumbnail: null ,
+      srcThumbnail: null,
       isUploading: false,
-      active: false
+      active: false,
+      error: null
     }
   }
 
@@ -38,7 +47,11 @@ class UploadForm extends Component {
   }
 
   handleThumbnailChange = (event) => {
-    this.setState({thumbnailFile: event.target.files[0], srcThumbnail: URL.createObjectURL(event.target.files[0])})
+    if (event.target.files[0].size < 1024 * 1024) {
+      this.setState({thumbnailFile: event.target.files[0], srcThumbnail: URL.createObjectURL(event.target.files[0]), error: null})
+    } else {
+      this.setState({ error: 'Thumbnail file should be smaller than 1MB.' })
+    }
   }
 
   handleSubmit (event) {
@@ -66,30 +79,33 @@ class UploadForm extends Component {
       isUploading
       ? <Loader size='massive'>Uploading file...</Loader>
       : <Modal.Content>
+        {this.state.error ? <Message content={this.state.error} negative /> : null}
         <Grid>
           <Grid.Row>
             <Grid.Column width={5}>
               {/* Select thumbnail */}
               <Dimmer.Dimmable
                 dimmed={active}
-                style={{ width: '240px', height: '196px'}}
+                style={{ width: '240px', height: '196px' }}
                 onMouseLeave={this.handleHide}
                 onMouseEnter={this.handleShow} >
-                  <Dimmer active={active} content={content} />
-                  <Image
-                    id='thumbnail'
-                    style={{ width: '240px', height: '196px'}}
-                    src={this.state.srcThumbnail || 'assets/img/thumbnail.png'} />
-                </Dimmer.Dimmable>
-                <small>Click to select a thumbnail for your song (240x196).</small>
+
+                <Dimmer active={active} content={content} />
+                <Image
+                  id='thumbnail'
+                  style={{ width: '240px', height: '196px' }}
+                  src={this.state.srcThumbnail || 'assets/img/thumbnail.png'} />
+
+              </Dimmer.Dimmable>
+              <small>Click to select a thumbnail for your song (240x196).</small>
             </Grid.Column>
             <Grid.Column width={11}>
               <Form id='uploadForm' onSubmit={this.handleSubmit}>
                 <Form.Field>
-                  <Form.Input id='fileSelector' type='file' style={{display:'none'}} value={this.state.thumbnail} onChange={this.handleThumbnailChange} />
+                  <input id='fileSelector' type='file' accept='.jpg,.png' style={{ display: 'none' }} value={this.state.thumbnail} onChange={this.handleThumbnailChange} />
                   <Form.Input label='Artist' type='text' value={this.state.artist} onChange={this.handleArtistChange} />
                   <Form.Input label='Title' type='text' value={this.state.title} onChange={this.handleTitleChange} />
-                  <Form.Input label='File' type='file' onChange={this.handleFileChange} />
+                  <input label='File' type='file' accept='audio/*' onChange={this.handleFileChange} />
                 </Form.Field>
                 <Form.Button>Submit</Form.Button>
               </Form>
