fe226919709a371718db088fd39f48370a2a9f9b
rllola <rllola80@gmail.com>
2018-04-04 22:31:10 +0200

    Download sie first

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,6 @@ echo "===== DEPLOY ====="
 cd ../ZeroNet-master
 python zeronet.py siteSign 1MzSDy3x8cFrt3P2YkXNN7ogYDFfKX6hnP $ZERONET_KEY_STAGING
 python zeronet.py sitePublish 1MzSDy3x8cFrt3P2YkXNN7ogYDFfKX6hnP 51.15.214.168
-timeout 300 python zeronet.py
 
 echo "===== EXIT ====="
 exit 0
