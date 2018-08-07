70b63aaf6be870ebbcfb5773de3b51c2b436c1ec
Lola Dam <rllola80@gmail.com>
2018-08-07 21:45:53 +0200

    Added donation text (#33)

--- app/scenes/Home/Home.js
+++ app/scenes/Home/Home.js
@@ -92,7 +92,7 @@ class Home extends Component {
             <Grid>
               <Grid.Column width={6}>
                 <p style={{ fontSize: '1.1em', color: 'rgba(0,0,0,.6)' }}>
-                  Mixtape is a playlist sharing platform created with the purpose of sharing your favorite music with friends and strangers. It also allow building playlist with several of your friends which will be grouped in a "hub".
+                  Mixtape is a playlist sharing platform created with the purpose of sharing your favorite music with friends and strangers. It also allow building playlist with several of people which will be grouped in a "hub".
                 </p>
                 <Header as='h4'>Why ?</Header>
                 <p style={{ fontSize: '1.1em', color: 'rgba(0,0,0,.6)' }}>
@@ -102,6 +102,33 @@ class Home extends Component {
               <Grid.Column width={10}>
                 <Image src='assets/img/Mixtape.png' fluid />
               </Grid.Column>
+            </Grid>
+          </Container>
+        </Segment>
+
+        <Segment style={{ padding: '8em 0em' }} vertical>
+          <Container>
+            <Grid>
+              <Grid.Row columns={3}>
+                <Grid.Column textAlign='center'>
+                  <Image src='assets/img/bitcoin-qr-code.png' centered />
+                  <p>1BzrPMr7qrca2wMV4a1qeH4CmJEKtAbrKW</p>
+                </Grid.Column>
+                <Grid.Column textAlign='center'>
+                  <Image src='assets/img/dogecoin-qr-code.png' centered />
+                  <p>DR252U5g3DECTpXsx8t4PBaePgepD1oADw</p>
+                </Grid.Column>
+                <Grid.Column>
+                  <Header as='h3' style={{ fontSize: '2em' }}>Donation</Header>
+                  <p style={{ fontSize: '1.1em', color: 'rgba(0,0,0,.6)' }}>
+                    You can help me to improve Mixtape by doing a donation. Those donations would not only be use for Mixtape, I will also dedicate some of my times writting on my experience/good practice/libraries/plugins. As much as possible I will engage myself in giving back to the Zeronet community by focusing on documenting my work.
+                  </p>
+                  <Header as='h4'>Why Dogecoin ?</Header>
+                  <p style={{ fontSize: '1.1em', color: 'rgba(0,0,0,.6)' }}>
+                    I dunno. Fun.
+                  </p>
+                </Grid.Column>
+              </Grid.Row>
             </Grid>
           </Container>
         </Segment>
